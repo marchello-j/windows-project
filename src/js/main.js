020d1e44fcc665264d37b8de9d8a6f79443aabce
Marsel <marchello.marsel@gmail.com>
2022-08-31 12:27:25 +0600

    добавили немного логики в modals.js для отображения модального окна при расчете, в index.html добавили data атрибуты для удобного манипулированиями модальными окнами

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,5 @@ window.addEventListener("DOMContentLoaded", () => {
 	modals();
 	tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
 	tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
-	forms()
+	forms();
 });
