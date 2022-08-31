2ee9afa64dd86acade6b708da3b24355da0aa026
Marsel <marchello.marsel@gmail.com>
2022-08-31 14:33:04 +0600

    реализовал таймер на сайте

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener("DOMContentLoaded", () => {
 	"use strict";
 
 	let modalState = {};
+	let dedline = "2022-09-20";
 
 	changeModalState(modalState);
 
@@ -15,4 +17,5 @@ window.addEventListener("DOMContentLoaded", () => {
 	tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
 	tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
 	forms(modalState);
+	timer(".container1", dedline);
 });
